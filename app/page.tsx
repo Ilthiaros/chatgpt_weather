86fcd8643a8629c16fbdb7fabb08578a5a123ef0
William Fischer <william.fischer@immersight.de>
2023-09-09 22:28:45 +0200

    added select comp, heroicons

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,34 @@
-'use client';
+"use client";
 
-import { Card, Divider, Title, Subtitle, Text, ScatterChart } from "@tremor/react";
+import CityPicker from "@/components/CityPicker";
+import {
+  Card,
+  Divider,
+  Title,
+  Subtitle,
+  Text,
+  ScatterChart,
+} from "@tremor/react";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E]
-     p-10 flex flex-col justify-center items-center">
+    <div
+      className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E]
+     p-10 flex flex-col justify-center items-center"
+    >
       <Card className="max-w-4xl mx-auto">
-        <Text className="text-6xl font-bold text-center mb-10">Smart Weather</Text>
+        <Text className="text-6xl font-bold text-center mb-10">
+          Smart Weather
+        </Text>
         <Subtitle className="text-xl text-center">
           Powered by OpenAI, Next,js 13, TailwindCSS, Tremor, and More!
         </Subtitle>
 
         <Divider className="my-10" />
         <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E]">
-          {/*City Picker*/}
+          <CityPicker />
         </Card>
       </Card>
-
     </div>
-  )
+  );
 }
